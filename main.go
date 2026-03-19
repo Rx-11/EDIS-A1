8cc19e62bca92f35dcc0022cb9d9b3093f7952a8
Rx-11 <triksters123@gmail.com>
2026-03-19 11:42:53 -0400

    added main.go

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"strings"
+
+	"github.com/Rx-11/EDIS-A1/config"
+	"github.com/Rx-11/EDIS-A1/db"
+	"github.com/Rx-11/EDIS-A1/public"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	app := fiber.New(fiber.Config{
+		AppName: "BookStore-Backend",
+	})
+
+	app.Use(logger.New(logger.Config{Next: func(c *fiber.Ctx) bool {
+		return strings.HasPrefix(c.Path(), "/")
+	}}))
+
+	config.Init()
+	log.Println("Loaded configs.")
+	db.Init(config.GetConfig().DbConfig, db.MySQL, db.LogInfo)
+	db.Migrate()
+
+	public.MountRoutes(app)
+
+	log.Println("Server started at http://localhost:3000")
+	log.Fatal(app.Listen("0.0.0.0:3000"))
+
+}
